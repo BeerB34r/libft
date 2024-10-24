47c3d0f9af5afc0a5f257ad10171f15bbca053b8
Mats De Beer <mde-beer@f0r2s5.codam.nl>
2024-10-24 18:15:15 +0200

    slight change to putstr

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,14 +6,14 @@
 /*   By: mde-beer <marvin@42.fr>                       +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2024/10/10 15:38:27 by mde-beer       #+#    #+#                */
-/*   Updated: 2024/10/10 17:12:05 by mde-beer       ########   odam.nl        */
+/*   Updated: 2024/10/16 13:11:00 by mde-beer       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <unistd.h>
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	while (*s)
-		ft_putchar_fd(*s++, fd);
+	write(fd, s, ft_strlen(s));
 }
